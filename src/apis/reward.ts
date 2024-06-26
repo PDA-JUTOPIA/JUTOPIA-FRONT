bc13cb2e0d44b6208acfe484cdc76d5c69b27bb2
ymkdev <eveminful@gmail.com>
2024-06-26 19:11:47 +0900

    feat: 리워드 페이지, 마이페이지 화면 리워드 api 연결

---
+++ src/apis/reward.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosResponse } from "axios";
+import Error from "next/error";
+
+const fullApiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export interface IResReadAll {
+  rewards: Reward[];
+}
+
+export interface Reward {
+  reward_id: number;
+  reward_name: string;
+  reward_explain: string;
+  reward_img_url: string;
+}
+
+export interface IResReadOne {
+  reward: {
+    reward_id: number;
+    reward_name: string;
+    reward_explain: string;
+    reward_img_url: string;
+  };
+}
+
+export async function readRewards(): Promise<Reward[]> {
+  try {
+    const resp: AxiosResponse<Reward[]> = await axios.get(
+      `${fullApiUrl}/api/reward/read`,
+    );
+    return resp.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Failed to read Rewards (AxiosError):", error.message);
+    } else if (error instanceof Error) {
+      console.error("Failed to read Rewards:", error);
+    } else {
+      console.error("Failed to read Rewards: An unknown error occurred.");
+    }
+  }
+}
+
+export async function readRewardOne(reward_id: number): Promise<IResReadOne> {
+  try {
+    const resp: AxiosResponse<IResReadOne> = await axios.get(
+      `${fullApiUrl}/api/reward/${reward_id}`,
+    );
+
+    return resp.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Failed to read Reward (AxiosError):", error.message);
+    } else if (error instanceof Error) {
+      console.error("Failed to read Reward:", error);
+    } else {
+      console.error("Failed to read Reward: An unknown error occurred.");
+    }
+  }
+}
--- src/components/ChallengeGrid.tsx
+++ src/components/ChallengeGrid.tsx
@@ -1,13 +1,58 @@
 // components/ChallengeGrid.tsx
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { readUserRewards } from "~/apis/userReward";
+import { readRewards } from "~/apis/reward";
 import RewardCard from "~/components/RewardCard";
-import { challenges } from "~/data/challengesData";
+// import { challenges } from "~/data/challengesData";
 
 interface ChallengeGridProps {
   showOnlyCompleted?: boolean;
+  email: string;
 }
 
-const ChallengeGrid: React.FC<ChallengeGridProps> = ({ showOnlyCompleted }) => {
+const ChallengeGrid: React.FC<ChallengeGridProps> = ({
+  showOnlyCompleted,
+  email,
+}) => {
+  interface Reward {
+    reward_id: number;
+    reward_name: string;
+    reward_explain: string;
+    reward_img_url: string;
+    completed?: boolean;
+  }
+  const [challenges, setChallenges] = useState<Reward[]>([]);
+  const [modalStates, setModalStates] = useState<boolean[]>([]);
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      try {
+        const response = await readRewards();
+        const initialChallenges = response.map((challenge) => ({
+          ...challenge,
+          completed: false,
+        }));
+
+        const rewardsArray = await readUserRewards(email); // Reward[] 타입의 배열을 받아옴
+
+        const updatedChallenges = initialChallenges.map((challenge) => ({
+          ...challenge,
+          completed: rewardsArray.userRewardIds.includes(challenge.reward_id),
+        }));
+
+        setChallenges(updatedChallenges);
+
+        setModalStates(updatedChallenges.map(() => false));
+      } catch (error) {
+        console.error("Failed to fetch initial rewards:", error);
+      }
+    }
+
+    fetchInitialData().catch((error) => {
+      console.error("Unhandled error:", error);
+    });
+  }, [email]);
+
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {challenges
@@ -15,10 +60,17 @@ const ChallengeGrid: React.FC<ChallengeGridProps> = ({ showOnlyCompleted }) => {
         .map((challenge, index) => (
           <RewardCard
             key={index}
-            title={challenge.title}
-            imageUrl={challenge.imageUrl}
-            description={challenge.description}
+            title={challenge.reward_name}
+            imageUrl={challenge.reward_img_url}
+            description={challenge.reward_explain}
             completed={challenge.completed}
+            showDetails={true}
+            showModal={modalStates[index]}
+            setShowModal={(value) => {
+              const newModalStates = [...modalStates];
+              newModalStates[index] = value;
+              setModalStates(newModalStates);
+            }}
           />
         ))}
     </div>
