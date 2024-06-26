38988916ac4a74b801df7539120e46f1c612bf4a
liarcrown <liarcrown@naver.com>
2024-06-26 10:37:50 +0900

    feat: 챌린지 페이지 동적 라우팅 추가

--- src/components/ChallengeExplain.tsx
+++ src/components/ChallengeExplain.tsx
@@ -1,37 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import image from "/public/bgimage.jpg";
 import PostCard from "./ChallengePostCard";
 import ChallengePostModal from "./ChallegePostModal";
-import { readAllRecurit, IResChallengeRecruit } from "~/apis/challenge";
+// import { readAllRecurit, IResChallengeRecruit } from "~/apis/challenge";
 
-const ChallengeExplain: React.FC = () => {
+interface ChallengeExplainProps {
+  challenge: {
+    challenge_id: number;
+    challenge_name: string;
+    challenge_detail: string;
+    challenge_thumbnail: string;
+    challenge_total: number;
+    challenge_recurit_start: string;
+    challenge_recurit_end: string;
+    challenge_start: string;
+    challenge_end: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
+
+const ChallengeExplain: React.FC<ChallengeExplainProps> = ({ challenge }) => {
   const [activeTab, setActiveTab] = useState("내 인증 현황");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [challenges, setChallenges] = useState<IResChallengeRecruit[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchChallenges = async () => {
-      try {
-        const fetchedChallenges = await readAllRecurit();
-        setChallenges(fetchedChallenges);
-      } catch (error) {
-        console.error("Error fetching challenges:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchChallenges().catch((error) => {
-      console.error("Error in fetchChallenges:", error);
-    });
-  }, []); // [] 안에 의존성 배열을 빈 배열로 설정하여 한 번만 데이터를 로드하도록 설정
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // [] 안에 의존성 배열을 빈 배열로 설정하여 한 번만 데이터를 로드하도록 설정
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
@@ -55,7 +49,7 @@ const ChallengeExplain: React.FC = () => {
               userAvatar="/Reward/chart.svg"
               postTime="2024-06-23"
               postContent="챌린지 인증용 UI"
-              postImages={["/Reward/coin1.svg", "/Reward/coin2.svg"]}
+              postImages={["/Reward/coin1.svg"]}
               commentsCount={4}
               likesCount={3}
             />
@@ -64,17 +58,42 @@ const ChallengeExplain: React.FC = () => {
       case "참가자 인증 현황":
         return (
           <div className="flex flex-col space-y-4">
-            <ul>
-              {challenges.map((challenge) => (
-                <li key={challenge.challenge_id}>
-                  <h3>{challenge.challenge_name}</h3>
-                  <p>{challenge.challenge_detail}</p>
-                  <p>Start: {challenge.challenge_start}</p>
-                  <p>End: {challenge.challenge_end}</p>
-                  {/* 필요한 정보들을 활용하여 UI를 구성 */}
-                </li>
-              ))}
-            </ul>
+            <PostCard
+              userName="윤경"
+              userAvatar="/Reward/chart.svg"
+              postTime="2024-06-23"
+              postContent="챌린지 인증용 UI"
+              postImages={["/Reward/coin1.svg"]}
+              commentsCount={4}
+              likesCount={3}
+            />
+            <PostCard
+              userName="윤경"
+              userAvatar="/Reward/chart.svg"
+              postTime="2024-06-23"
+              postContent="챌린지 인증용 UI"
+              postImages={["/Reward/coin1.svg"]}
+              commentsCount={4}
+              likesCount={3}
+            />
+            <PostCard
+              userName="윤경"
+              userAvatar="/Reward/chart.svg"
+              postTime="2024-06-23"
+              postContent="챌린지 인증용 UI"
+              postImages={["/Reward/coin1.svg"]}
+              commentsCount={4}
+              likesCount={3}
+            />
+            <PostCard
+              userName="윤경"
+              userAvatar="/Reward/chart.svg"
+              postTime="2024-06-23"
+              postContent="챌린지 인증용 UI"
+              postImages={["/Reward/coin1.svg"]}
+              commentsCount={4}
+              likesCount={3}
+            />
           </div>
         );
       default:
@@ -86,29 +105,41 @@ const ChallengeExplain: React.FC = () => {
     <div className="flex flex-col space-y-6 bg-gray-100 p-6 lg:flex-row lg:space-x-6 lg:space-y-0">
       <div className="flex flex-col space-y-6 rounded-lg bg-white p-6 shadow-lg lg:w-1/2">
         <div className="flex flex-col items-center justify-between space-y-4">
-          <h1 className="text-5xl font-medium text-slate-900">클릭한 챌린지</h1>
+          <h1 className="text-5xl font-medium text-slate-900">
+            {challenge.challenge_name}
+          </h1>
           <Image
-            src={image}
-            alt="Background Image"
+            src={challenge.challenge_thumbnail}
+            alt={challenge.challenge_thumbnail}
             width={500}
             height={500}
             className="rounded-lg shadow-md"
           />
         </div>
         <div className="text-center text-3xl font-bold text-gray-600 lg:text-left">
-          주식 어린이, 주린이를 탈출합시다! 꾸준히 참여만 해도 주식 바보
-          탈출하기 챌린지😄
+          {challenge.challenge_detail}
         </div>
         <div>
-          <p className="text-center text-sm text-slate-500 lg:text-left">
-            🔥주토피아 자체 제공하는 챌린지입니다.
-          </p>
-          <button
-            className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-            onClick={handleModalOpen}
-          >
-            인증글 작성하기
-          </button>
+          <div className="text-center text-sm text-slate-500 lg:text-left">
+            <p>
+              기간 : {new Date(challenge.challenge_start).toLocaleString()} ~{" "}
+              {new Date(challenge.challenge_end).toLocaleString()}
+            </p>
+          </div>
+          <div className="flex flex-row justify-around">
+            <button
+              className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+              onClick={handleModalOpen}
+            >
+              인증글 작성하기
+            </button>
+            <button
+              className="mt-4 rounded-lg bg-red-400 px-4 py-2 text-white hover:bg-red-600"
+              onClick={handleModalOpen}
+            >
+              챌린지 참여하기
+            </button>
+          </div>
         </div>
       </div>
       <div className="lg:w-1/2">
---
+++ src/pages/challenge-detail/[challengeId].tsx
@@ -0,0 +1,71 @@
+// pages/challenge-detail/[challengeId].tsx
+
+/* eslint-disable @next/next/no-img-element */
+import { NextPage, GetServerSideProps } from "next";
+import { useState, useEffect } from "react";
+import TopBar from "~/components/TopBar";
+import { LeftBar } from "~/components/LeftBar";
+import { BottomBar } from "~/components/BottomBar";
+import ChallengeExplain from "~/components/ChallengeExplain";
+
+interface ChallengeDetailProps {
+  challenge: {
+    challenge_id: number;
+    challenge_name: string;
+    challenge_detail: string;
+    challenge_thumbnail: string;
+    challenge_total: number;
+    challenge_recurit_start: string;
+    challenge_recurit_end: string;
+    challenge_start: string;
+    challenge_end: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
+
+const ChallengeDetail: NextPage<ChallengeDetailProps> = ({ challenge }) => {
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  if (!isHydrated) {
+    return null;
+  }
+
+  return (
+    <div className="font-ttlaundrygothicb">
+      <TopBar menuName="챌린지" />
+      <LeftBar selectedTab="챌린지" />
+
+      <div className="flex justify-evenly pt-10 md:ml-[8rem] lg:ml-64 lg:gap-6">
+        <div className="flex w-full flex-col justify-around gap-5 p-2">
+          <ChallengeExplain challenge={challenge} />
+        </div>
+      </div>
+
+      <div className="pt-[90px]"></div>
+
+      <BottomBar selectedTab="챌린지" />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { challengeId } = context.params as { challengeId: string };
+
+  const res = await fetch(
+    `http://localhost:3001/api/challenge/readChallenge/challengeId/${challengeId}`,
+  );
+  const challenge = await res.json();
+
+  return {
+    props: {
+      challenge,
+    },
+  };
+};
+
+export default ChallengeDetail;
