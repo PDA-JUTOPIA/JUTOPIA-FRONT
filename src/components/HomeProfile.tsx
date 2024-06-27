09a34b3518b2a3d89ef51f97cc7214dfb9aa1cda
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-27 16:50:50 +0900

    feat: 홈 화면에서 튜토리얼 진행 현황 확인하는 api 반영

--- src/components/HomeProfile.tsx
+++ src/components/HomeProfile.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useBoundStore } from "~/hooks/useBoundStore";
-import { readSinceJoinDate } from "~/apis/user";
+import { readSinceJoinDate, readUserLearningStatus } from "~/apis/user";
 import Image from "next/image";
 
 const Profile: React.FC = () => {
   const username = useBoundStore((x) => x.name);
   const email = useBoundStore((x) => x.email);
   const [daysSinceJoined, setDaysSinceJoined] = useState<number | null>(null);
+  const [tutorialStatus, setTutorialStatus] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchDaysSinceJoined() {
@@ -19,7 +20,21 @@ const Profile: React.FC = () => {
       }
     }
 
+    async function fetchTutorialStatus() {
+      try {
+        const response = await readUserLearningStatus(email);
+        setTutorialStatus(response.learningStatus);
+      } catch (error) {
+        console.error("Error fetching tutorial status:", error);
+        // 에러 처리 로직을 추가할 수 있습니다.
+      }
+    }
+
     fetchDaysSinceJoined().catch((error) => {
+      console.error("Unhandled error:", error);
+    });
+
+    fetchTutorialStatus().catch((error) => {
       console.error("Unhandled error:", error);
     });
   }, [email]);
@@ -39,7 +54,10 @@ const Profile: React.FC = () => {
           주토피아와 함께한 지 {daysSinceJoined}일
         </h2>
         <h2 className="mt-2 text-sm font-semibold sm:text-lg">
-          이번 주 총 N일 학습 완🔥
+          튜토리얼 {tutorialStatus} 완🔥
+        </h2>
+        <h2 className="mt-2 text-sm font-semibold sm:text-lg">
+          이번 주 총 N일 챌린지 인증 완🔥
         </h2>
       </div>
     </div>
